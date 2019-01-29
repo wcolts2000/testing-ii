f175c4a45d182a4c29362e1baf1d70da22cb0555
wcolts2000 <wcolts2000@gmail.com>
2019-01-29 12:33:06 -0800

    implemented state and methods on dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,36 @@ import Dashboard from "./components/Dashboard";
 import Display from "./components/Display";
 
 class App extends Component {
+  state = {
+    balls: 0,
+    strikes: 0,
+    fouls: 0
+  };
+
+  handleHit = () => {
+    this.setState({
+      balls: 0,
+      strikes: 0,
+      fouls: 0
+    });
+  };
+
+  handleBoardChange = name => {
+    let prevState = this.state[name];
+    console.log(prevState);
+
+    this.setState({ [name]: prevState + 1 });
+  };
+
   render() {
     return (
       <div className="App">
         <div>Hello World</div>
-        <Dashboard />
-        <Display />
+        <Dashboard
+          handleBoardChange={this.handleBoardChange}
+          handleHit={this.handleHit}
+        />
+        <Display currentState={this.state} />
       </div>
     );
   }
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { handleBoardChange, handleHit } = this.props;
     return (
       <div>
         <h1>Dashboard</h1>
+        <button onClick={() => handleBoardChange("balls")}>Balls</button>
+        <button onClick={() => handleBoardChange("strikes")}>Strikes</button>
+        <button onClick={() => handleBoardChange("fouls")}>Fouls</button>
+        <button onClick={() => handleHit()}>hit</button>
       </div>
     );
   }
