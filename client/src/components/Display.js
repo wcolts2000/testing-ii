e004b1624a50ac89061cc16668c26013096cf4f8
wcolts2000 <wcolts2000@gmail.com>
2019-01-29 14:38:36 -0800

    testing display components rendering what its supposted to

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,38 +1,3 @@
-// import React, { Component } from "react";
-
-// export default class Display extends Component {
-//   render() {
-//     const { balls, strikes, fouls } = this.props.currentState;
-//     return (
-//       <div>
-//         <h1 data-testid="display">Display</h1>
-//         <ul>
-//           <li>Balls: {balls}</li>
-//           <li>Strikes: {strikes}</li>
-//           <li>Fouls: {fouls}</li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// import React from "react";
-
-// function Display(props) {
-//   const { balls, strikes, fouls } = props.currentState;
-//   return (
-//     <div>
-//       <h1 data-testid="display">Display</h1>
-//       <ul>
-//         <li>Balls: {balls}</li>
-//         <li>Strikes: {strikes}</li>
-//         <li>Fouls: {fouls}</li>
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default Display;
 import React from "react";
 
 function Display(props) {
@@ -43,10 +8,10 @@ function Display(props) {
   return (
     <div>
       <h1 data-testid="display">Display</h1>
-      <ul>
-        <li>Balls: {balls}</li>
-        <li>Strikes: {strikes}</li>
-        <li>Fouls: {fouls}</li>
+      <ul data-testid="list">
+        <li data-testid="balls">Balls: {balls}</li>
+        <li data-testid="strikes">Strikes: {strikes}</li>
+        <li data-testid="fouls">Fouls: {fouls}</li>
       </ul>
     </div>
   );
--- client/src/components/Display.spec.js
+++ client/src/components/Display.spec.js
@@ -5,23 +5,18 @@ import Display from "./Display";
 
 afterEach(cleanup);
 
-// describe("<Display/>", () => {
-//   it("renders the display component", async () => {
-//     const { getByTestId, debug } = render(<Display />);
-//     debug();
-//     const title = await waitForElement(() => {
-//       getByTestId("display");
-//     });
-
-//     expect(getByTestId("display")).toHaveTextContent("Display");
-//   });
-// });
 describe("<Display/>", () => {
-  test("renders the display component", async () => {
+  it("renders the display component", async () => {
     const { getByTestId, debug } = await render(<Display currentState />);
     debug();
     const title = getByTestId("display");
+    const balls = getByTestId("balls");
+    const strikes = getByTestId("strikes");
+    const fouls = getByTestId("fouls");
 
     expect(title).toHaveTextContent(/display/i);
+    expect(balls).toHaveTextContent(/balls/i);
+    expect(strikes).toHaveTextContent(/strikes/i);
+    expect(fouls).toHaveTextContent(/fouls/i);
   });
 });
